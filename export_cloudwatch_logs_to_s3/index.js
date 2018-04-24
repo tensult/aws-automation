1f3262167c1a464398250245602954b8ea149d62
parag poddar <paragpoddar01@gmail.com>
2018-04-24 15:10:02 +0530

    Modify index.js Create s3_bucket_policy.json and step_function.json

--- export_cloudwatch_logs_to_s3/index.js
+++ export_cloudwatch_logs_to_s3/index.js
@@ -23,13 +23,18 @@ function getDatePath(date) {
 }
 
 function getLogPathForS3(logGroupName) {
-    const logPathParts = logGroupName.split('/');
-    const mainParts = logPathParts[0].split('-');
-    let logPath = mainParts.slice(1).join('/');
-    if (logPathParts.length > 1) {
-        logPath = logPath + '-' + logPathParts.slice(1).join('-');
+    if(logGroupName.startsWith('/')) {
+        logGroupName = logGroupName.slice(1);
     }
-    return logPath;
+    return logGroupName.replace(/\//g, '-');
+}
+
+function wait(timeout) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve()
+        }, timeout)
+    })
 }
 
 function describeExportTask(taskId) {
@@ -39,41 +44,44 @@ function describeExportTask(taskId) {
     return cloudWatchLogs.describeExportTasks(params).promise();
 }
 
-function waitForExportTaskToComplete(taskId) {
-    return new Promise((resolve, reject) => {
-        describeExportTask(taskId)
-            .then((response) => {
-                let task = response.exportTasks[0];
-                let taskStatus = task.status.code;
-                if (taskStatus === 'RUNNING' || taskStatus.indexOf('PENDING')) {
-                    console.log('Task is running for ', task.logGroupName);
-                    setTimeout(() => {
-                        waitForExportTaskToComplete(taskId).then(resolve).catch(reject);
-                    }, 1000);
-                }
-                else {
-                    console.log('Task is completed for ', task.logGroupName);
-                    resolve();
-                }
-            })
-            .catch(reject)
-    })
+let waitErrorCount = 0;
+async function waitForExportTaskToComplete(taskId) {
+    try {
+        const taskDetails = await describeExportTask(taskId);
+        let task = taskDetails.exportTasks[0];
+        let taskStatus = task.status.code;
+        if (taskStatus === 'RUNNING' || taskStatus.indexOf('PENDING') !== -1) {
+            console.log('Task is running for ', task.logGroupName, 'with stats', task.status);
+            await wait(1000);
+            return await waitForExportTaskToComplete(taskId);
+        }
+        return true;
+    } catch (error) {
+        waitErrorCount++;
+        if (waitErrorCount < 3) {
+            return await waitForExportTaskToComplete(taskId);
+        }
+        throw error;
+    }
 }
 
-function exportToS3Task(s3BucketName, logGroupName, logFolderName) {
-    const logPathForS3 = getLogPathForS3(logGroupName);
-    const yesterdayDate = getYesterdayDate();
-    let params = {
-        destination: s3BucketName,
-        destinationPrefix: `${logPathForS3}/${getDatePath(yesterdayDate)}`,
-        from: getDayStartTimestamp(getYesterdayDate()),
-        logGroupName: logGroupName,
-        to: getDayStartTimestamp()
-    };
-    return cloudWatchLogs.createExportTask(params).promise()
-        .then((response) => {
-            return waitForExportTaskToComplete(response.taskId);
-        });
+async function exportToS3Task(s3BucketName, logGroupName, logFolderName) {
+    try {
+        const logPathForS3 = getLogPathForS3(logGroupName);
+        const yesterdayDate = getYesterdayDate();
+        let params = {
+            destination: s3BucketName,
+            destinationPrefix: `${logFolderName}/${logPathForS3}/${getDatePath(yesterdayDate)}`,
+            from: getDayStartTimestamp(getYesterdayDate()),
+            logGroupName: logGroupName,
+            to: getDayStartTimestamp()
+        };
+        // console.log(params);
+        const response = await cloudWatchLogs.createExportTask(params).promise();
+        await waitForExportTaskToComplete(response.taskId);
+    } catch (error) {
+        throw error;
+    }
 }
 
 function getCloudWatchLogGroups(nextToken, limit) {
@@ -84,15 +92,19 @@ function getCloudWatchLogGroups(nextToken, limit) {
     return cloudWatchLogs.describeLogGroups(params).promise();
 }
 
+// Pass s3BucketName, logFolderName and logGroupFilter in event
 exports.handler = async (event) => {
     let s3BucketName = event.s3BucketName;
     let logFolderName = event.logFolderName;
+    let nextToken = event.nextToken;
+    let logGroupFilter = event.logGroupFilter;
     try {
         let cloudWatchLogGroups = await getCloudWatchLogGroups(nextToken, 1);
         event.nextToken = cloudWatchLogGroups.nextToken;
         event.continue = cloudWatchLogGroups.nextToken !== undefined;
         const logGroupName = cloudWatchLogGroups.logGroups[0].logGroupName;
-        if(logGroupName.toLowerCase().indexOf('prod') < 0) {
+        if (logGroupFilter && logGroupName.toLowerCase().indexOf(logGroupFilter) < 0) {
+            // Ignore log group
             return event;
         }
         await exportToS3Task(s3BucketName, logGroupName, logFolderName);
