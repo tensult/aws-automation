425814854b3cc115fa496d1319997d85ee847412
parag poddar <paragpoddar01@gmail.com>
2018-06-19 17:57:33 +0530

    Add manage security group shell script

---
+++ manage-security-groups.sh
@@ -0,0 +1,45 @@
+for i in "$@"
+do
+case $i in 
+    -s=*|--status=*)
+    status="${i#*=}"
+    shift
+    ;;
+    -p=*|--profile=*)
+    profile="${i#*=}"
+    shift
+    ;;
+    -P=*|--port=*)
+    port="${i#*=}"
+    shift
+    ;;
+    -g=*|--group=*)
+    group="${i#*=}"
+    shift
+    ;;
+esac
+done
+if [ -z "$profile" ]
+then
+    echo "Pass AWS profile name using --profile=<profile-name> or -p=<profile-name>"
+    exit -1
+elif [ -z "$status" ]
+then
+    echo "Pass status using --status=<status> or -s=<status>"
+    exit -1
+elif [ -z "$port" ]
+then
+    echo "Pass port using --port=<port> or -P=<port>"
+    exit -1
+elif [ -z "$group" ]
+then
+    echo "Pass group using --group=<group> or -g=<group>"
+    exit -1
+fi
+if [ $status == "open" ]
+then
+aws ec2 --profile $profile authorize-security-group-ingress --group-name $group --protocol tcp --port $port --cidr 0.0.0.0/0
+elif [ $status == "close" ]
+then
+aws ec2 --profile $profile revoke-security-group-ingress --group-name $group --protocol tcp --port $port --cidr 0.0.0.0/0
+fi
