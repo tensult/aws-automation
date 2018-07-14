bd59a4703997699f0e5c84e09b7d9834d4386c97
parag poddar <paragpoddar01@gmail.com>
2018-07-14 23:45:58 +0530

    Add script to enable cloudwatch logging for apigateway apis

---
+++ enable_cloudwatch_logs_for_apis.js
@@ -0,0 +1,95 @@
+const AWS = require('aws-sdk');
+const ApiGateway = new AWS.APIGateway({ region: 'ap-south-1' });
+
+function wait(timeout) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve()
+        }, timeout)
+    })
+}
+
+function isEmpty(obj) {
+    for(var prop in obj) {
+        if(obj.hasOwnProperty(prop))
+            return false;
+    }
+    return JSON.stringify(obj) === JSON.stringify({});
+}
+
+// console.log(isEmpty({a:5}));
+
+// Deploy api to stage
+function createDeployment(restApiId, stageName) {
+    var params = {
+        restApiId, /* required */
+        stageName
+    };
+    return ApiGateway.createDeployment(params).promise();
+}
+
+function updateStage(restApiId, stageName) {
+    const params = {
+        restApiId, /* required */
+        stageName, /* required */
+        patchOperations: [
+            {
+                op: 'replace',
+                path: '/*/*/logging/loglevel',
+                value: 'INFO'
+            },
+            /* more items */
+        ]
+    };
+    return ApiGateway.updateStage(params).promise();
+}
+
+function hasLoggingEnabled(stage) {
+    // console.log(stage)
+    if (isEmpty(stage.methodSettings) || stage.methodSettings['*/*'].loggingLevel === 'OFF') {
+        return false;
+    }
+    return true;
+}
+
+function getRestApis() {
+    const params = {
+        // limit: 0,
+        // position: 'STRING_VALUE'
+    };
+    return ApiGateway.getRestApis(params).promise();
+}
+
+// Get info anout stage resource of api
+function getStages(restApiId) {
+    const params = {
+        restApiId, /* required */
+        // deploymentId: 'STRING_VALUE'
+    };
+    return ApiGateway.getStages(params).promise();
+}
+
+async function enableCloudWatchLogsHandler() {
+    const restApis = await getRestApis();
+    console.log('Rest APIs: ', restApis);
+    for (let i = 0; i < restApis.items.length; i++) {
+        const stages = await getStages(restApis.items[i].id);
+        console.log(`Rest API id is ${restApis.items[i].id} and stages: ${JSON.stringify(stages)}`);
+        for (let j = 0; j < stages.item.length; j++) {
+            const _hasLoggingEnabled = hasLoggingEnabled(stages.item[j]);
+            console.log('log enable status: ', _hasLoggingEnabled);
+            if (!_hasLoggingEnabled) {
+                const restApiId = restApis.items[i].id;
+                const stageName = stages.item[j].stageName;
+                console.log(`logging enabling starting for restapi id: ${restApiId} and stage: ${stageName}`);
+                const isLogggingEnabled = await updateStage(restApiId, stageName);
+                console.log('Log enabled response: ', JSON.stringify(isLogggingEnabled));
+                const deployedApi = await createDeployment(restApiId, stageName);
+                console.log('Deployed api response: ', deployedApi);
+                await wait(10000);
+            }
+        }
+    }
+}
+
+enableCloudWatchLogsHandler();
