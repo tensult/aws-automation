a7ec7c169c85b8f3742230575c9d1da6d0b3d253
Dilip Kola <dilip@tensult.com>
2018-10-27 10:23:25 +0530

    Fix command instruction

--- add-my-ip-to-security-group.sh
+++ add-my-ip-to-security-group.sh
@@ -5,4 +5,4 @@ ip=`curl -s https://api.ipify.org`
 aws ec2 authorize-security-group-ingress --protocol tcp --port 22 --cidr $ip/32 --group-id $1
 
 # To run:
-# AWS_PROFILE=aia-dev AWS_REGION=us-east-1 sh add-my-ip-to-security-group.sh <group-id>
+# AWS_PROFILE=<aws-profile> AWS_REGION=<aws-region> sh add-my-ip-to-security-group.sh <group-id>
