ebcb5d7baa8159ad62a1d951fda0e5cd4816dd83
agnel <agnel@Tensults-MacBook-Air.local>
2018-11-15 13:16:16 +0530

    Added try catch

--- send_ses_verification_email.js
+++ send_ses_verification_email.js
@@ -8,6 +8,7 @@ const cli = require('cli');
 const cliArgs = cli.parse({
     profile: ['p', 'AWS profile name', 'string', 'default'],
     region: ['r', 'AWS region', 'string'],
+    sesRegion : ['sr','SES service region', 'string'],
     email: ['e', "Email to be verified by SES", "string"],
 });
 
@@ -17,11 +18,18 @@ if (!cliArgs.profile || !cliArgs.region || !cliArgs.email) {
 
 awsConfigHelper.updateConfig(cliArgs.profile, cliArgs.region);
 
-const ses = new AWS.SES();
+const ses = new AWS.SES({
+    region : cliArgs.sesRegion
+});
 
 async function sendVerificationEmail() {
-    await ses.verifyEmailIdentity({
-        EmailAddress: cliArgs.email
-    }).promise();
+    try {
+        return await ses.verifyEmailIdentity({
+            EmailAddress: cliArgs.email
+        }).promise();
+    } catch (error) {
+        throw error;
+    };
 }
+
 sendVerificationEmail();
