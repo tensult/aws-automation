21d62c0b1690a52b3934067764c35d05d9b7fca7
parag poddar <paragpoddar01@gmail.com>
2019-02-21 13:34:26 +0530

    Add function to create sns topic and subscriptions

---
+++ create_sns_topic_and_subscriptions.js
@@ -0,0 +1,61 @@
+/**
+ * AWS_PROFILE=<aws-profile> node create_sns_topic_and_subsciptions.js -r <aws-region> -t '<topic-name>' -s '<email_1>,<email_2>,..' 
+ */
+
+const AWS = require('aws-sdk');
+const cli = require('cli');
+const awsConfigHelper = require('./util/awsConfigHelper');
+
+const cliArgs = cli.parse({
+    region: ['r', 'AWSRegion', 'string'],
+    topicName: ['t', 'TopicName', 'string'],
+    subscriptionEnpoints: ['s', 'SubscriptionEndpoints', 'string'],
+});
+
+if (!cliArgs.region || !cliArgs.topicName || !cliArgs.subscriptionEnpoints) {
+    cli.getUsage();
+}
+
+function getTopicName() {
+    return cliArgs.topicName;
+}
+
+function getSubscriptionEndpoints() {
+    return cliArgs.subscriptionEnpoints.split(',').map((endpoint) => {
+        return endpoint.trim();
+    })
+}
+
+function createSubscription(sns, topicArn, subscriptionEndpoint) {
+    const params = {
+        Protocol: 'email',
+        TopicArn: topicArn,
+        Endpoint: subscriptionEndpoint
+    };
+    return sns.subscribe(params).promise();
+}
+
+function createTopic(sns, topicName) {
+    const params = {
+        Name: topicName,
+    };
+    return sns.createTopic(params).promise();
+}
+
+async function handler() {
+    try {
+        await awsConfigHelper.updateConfig(cliArgs.region);
+        const sns = new AWS.SNS();
+        const topicName = getTopicName();
+        const createdTopic = await createTopic(sns, topicName);
+        const subscriptionEndpoints = getSubscriptionEndpoints();
+        for (const subscriptionEndpoint of subscriptionEndpoints) {
+            await createSubscription(sns, createdTopic.TopicArn, subscriptionEndpoint);
+        }
+    } catch (error) {
+        console.error(error);
+    }
+
+}
+
+handler();
