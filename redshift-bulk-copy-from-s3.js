2edc0392df7995758f0bef308de2e4cd40960e56
Dilip Kola <33080863+koladilip@users.noreply.github.com>
2019-04-07 14:32:21 +0530

    Create redshift-bulk-copy-from-s3.js
    Migrates data from S3 to RedShift using manifest files stored in S3

---
+++ redshift-bulk-copy-from-s3.js
@@ -0,0 +1,53 @@
+const pg = require("pg");
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3();
+
+let s3BucketName = process.argv[2];
+let s3FolderPrefix = process.argv[3];
+let redShiftEndPoint = process.argv[4];
+let redShiftDBName = process.argv[5];
+let redShiftDBUser = process.argv[6];
+let redShiftDBPassword = process.argv[7];
+let redShiftDBTableName = process.argv[8];
+let redShiftS3Role = process.argv[9]; // S3 bucket Read permissions should be granted to RedShift 
+
+const pool = new pg.Pool({
+    host: redShiftEndPoint,
+    database: redShiftDBName,
+    password: redShiftDBPassword,
+    port: 8192,
+    user: redShiftDBUser,
+});
+
+const connectionPromise = pool.connect();
+async function executeQuery(query, params) {
+    await connectionPromise;
+    const result = await pool.query(query, params)
+    console.log(result);
+}
+
+function listDayWiseObjects() {
+
+    return s3.listObjectsV2({
+        Bucket: s3BucketName,
+        Prefix: s3FolderPrefix
+    }).promise();
+}
+
+
+async function migrateToRedShift() {
+    const s3Response = await listDayWiseObjects();
+    if (s3Response.Contents && s3Response.Contents.length) {
+        for (let s3Object of s3Response.Contents) {
+            if (!s3Object.Key.endsWith("manifest")) {
+                console.log("Skipping", s3Object.Key);
+                continue;
+            }
+            console.log("Processing", s3Object.Key);
+            await executeQuery(`COPY ${redShiftDBTableName} FROM 's3://${s3BucketName}/${s3Object.Key}' CREDENTIALS 'aws_iam_role=${redShiftS3Role}' MANIFEST TRUNCATECOLUMNS TIMEFORMAT 'epochmillisecs'`);
+        }
+    }
+    console.log("Migration finished");
+}
+
+migrateToRedShift();
