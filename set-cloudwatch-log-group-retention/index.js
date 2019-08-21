4c3c2825d9c89f2432b0ed7c764234785911af62
parag poddar <paragpoddar01@gmail.com>
2019-08-21 16:51:39 +0530

    Update set-cloudwatch-log-group-retention/index.js

--- set-cloudwatch-log-group-retention/index.js
+++ set-cloudwatch-log-group-retention/index.js
@@ -1,9 +1,16 @@
 const AWS = require('aws-sdk');
 const cloudwatchLogs = new AWS.CloudWatchLogs();
 
+function describeLogGroup(logGroupName) {
+    return cloudwatchLogs.describeLogGroups({
+        limit: 1,
+        logGroupNamePrefix: logGroupName
+    }).promise()
+}
+
 function setRetentionOfCloudwatchLogGroup(logGroupName, duration) {
-    let params = {
-        logGroupName : logGroupName,
+    const params = {
+        logGroupName: logGroupName,
         retentionInDays: duration
     };
     return cloudwatchLogs.putRetentionPolicy(params).promise();
@@ -12,11 +19,15 @@ function setRetentionOfCloudwatchLogGroup(logGroupName, duration) {
 exports.handler = async (event) => {
     const logGroupName = event.logGroupName ? event.logGroupName : event.detail.requestParameters.logGroupName;
     try {
+        const logGroupInfo = await describeLogGroup(logGroupName);
+        if (logGroupInfo.logGroups[0].retentionInDays) {
+            console.log('Found retentionInDays set and so not setting retention');
+            return;
+        }
         await setRetentionOfCloudwatchLogGroup(logGroupName, 14);
         console.log('Retention has been set to ' + logGroupName + 'for 2 weeks');
         return;
-    } catch(error) {
-        console.error(error);
+    } catch (error) {
         throw error;
     }
 };
