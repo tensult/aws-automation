2f390a27b990b9094448e3d016e054fa7fb45945
parag poddar <paragpoddar01@gmail.com>
2019-10-28 15:34:12 +0530

    Update open_and_close_ec2_sg_ports.js

--- open_and_close_ec2_sg_ports.js
+++ open_and_close_ec2_sg_ports.js
@@ -1,5 +1,35 @@
+/**
+ * 
+ * @description
+ * This script helps to open and close ec2 instance security group port
+ * 
+ * How to use this script: - 
+ * 
+ * Install node and npm in your machine
+ * 
+ * Configure AWS profile in your machine
+ * https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-configure.html
+ * 
+ * Run the following commands to execute this script - 
+ * $ npm i aws-sdk
+ * $ IS_LOGGING_ALLOWED=1 INPUT_DATA="{\"publicIp\":\"<publicIp>\",\"fromPort\":\"<fromPort>\",\"ipProtocol\":\"<ipProtocol>\",\"cidrIp\":\"<cidrIp>\",\"toPort\":\"<toPort>\",\"action\":\"open\",\"region\":\"<region>\",\"description\":\"<description>\"}" AWS_PROFILE=<AWS_PROFILE> node open_and_close_sg_ports.js
+ * 
+ * 
+ */
+
 const AWS = require('aws-sdk');
 const sts = new AWS.STS();
+
+const {
+    INPUT_DATA,
+    IS_LOGGING_ALLOWED
+} = process.env;
+
+function logging(...s) {
+    if (IS_LOGGING_ALLOWED > '0') {
+        console.log(...s);
+    }
+}
 
 function revokeSecurityGroupIngress(ec2Obj, GroupId, CidrIp, sourceSg, FromPort, ToPort, IpProtocol) {
     const params = {};
@@ -86,44 +116,58 @@ function getAssumeRoleByRoleArn(roleArn) {
     return sts.assumeRole(params).promise();
 }
 
-async function handleOpeningAndClosingPorts(inputObj) {
+async function handleOpeningAndClosingPorts() {
     try {
+
+        const inputObj = JSON.parse(INPUT_DATA);
+        logging('Input object ', inputObj);
+
+        const region = inputObj.region;
         const action = inputObj.action;
         const roleArn = inputObj.roleArn;
         const publicIp = inputObj.publicIp;
         const privateIp = inputObj.privateIp;
-
         const fromPort = inputObj.fromPort;
         const ipProtocol = inputObj.ipProtocol;
         const cidrIp = inputObj.cidrIp;
         const toPort = inputObj.toPort;
         const description = inputObj.description;
         const sourceSgId = inputObj.sourceSgId;
-        let sgIds = (!inputObj.sgs || inputObj.sgs.length === 0) ? undefined : inputObj.sgs;
 
-        if (!action || !roleArn || (!publicIp && !privateIp) || !fromPort || !ipProtocol || (!cidrIp && !sourceSgId) || !toPort) {
+        let sgIds = (!inputObj.sgs || inputObj.sgs.length === 0) ? undefined : inputObj.sgs;
+        if (!action || (!publicIp && !privateIp) || !fromPort || !ipProtocol || (!cidrIp && !sourceSgId) || !toPort) {
             throw new Error('Required values missing!!');
         }
-
-        const assumeRoleRes = await getAssumeRoleByRoleArn(roleArn);
-        AWS.config.update({
-            accessKeyId: assumeRoleRes.Credentials.AccessKeyId,
-            secretAccessKey: assumeRoleRes.Credentials.SecretAccessKey,
-            sessionToken: assumeRoleRes.Credentials.SessionToken
+        // const assumeRoleRes = await getAssumeRoleByRoleArn(roleArn);
+        // AWS.config.update({
+        //     accessKeyId: assumeRoleRes.Credentials.AccessKeyId,
+        //     secretAccessKey: assumeRoleRes.Credentials.SecretAccessKey,
+        //     sessionToken: assumeRoleRes.Credentials.SessionToken
+        // });
+        const ec2Obj = new AWS.EC2({
+            region
         });
-        const ec2Obj = new AWS.EC2();
         const describeEc2InstanceRes = await describeEc2Instance(ec2Obj, publicIp);
+        if (describeEc2InstanceRes.Reservations.length === 0) {
+            throw new Error('No instance found in the region');
+        }
+        // logging('describeEc2InstanceRes', describeEc2InstanceRes);
         sgIds = describeEc2InstanceRes.Reservations[0].Instances[0].SecurityGroups.map(sg => sg.GroupId);
-        console.log(sgIds);
+        logging('Fetched SGs ', sgIds);
         const describeSgsRes = await describeSgs(ec2Obj, sgIds);
+        logging('Fetched SGs description ', describeSgsRes);
         if (action === 'close') {
             for (const sg of describeSgsRes.SecurityGroups) {
+                logging('got sg ', sg);
                 for (const ipPermission of sg.IpPermissions) {
-                    if (cidrIp && ipPermission.FromPort === fromPort && ipPermission.ToPort === toPort && ipPermission.IpRanges.length > 0) {
+                    logging('got ipPermission ', ipPermission);
+                    logging(cidrIp, ipPermission.FromPort === parseInt(fromPort), ipPermission.ToPort === parseInt(toPort), ipPermission.IpRanges.length > 0);
+                    if (cidrIp && ipPermission.FromPort === parseInt(fromPort) && ipPermission.ToPort === parseInt(toPort) && ipPermission.IpRanges.length > 0) {
+                        logging('condition passed');
                         for (const ipRange of ipPermission.IpRanges) {
                             if (ipRange.CidrIp === cidrIp) {
                                 await revokeSecurityGroupIngress(ec2Obj, sg.GroupId, ipRange.CidrIp, undefined, fromPort, toPort, ipProtocol);
-                                console.log('Yooooooooooo..... revoked security group ingress');
+                                logging('Yooooooooooo..... revoked security group ingress');
                             }
                         }
                     }
@@ -141,19 +185,12 @@ async function handleOpeningAndClosingPorts(inputObj) {
         if (action === 'open') {
             for (const sg of describeSgsRes.SecurityGroups) {
                 await authorizeSecurityGroupIngress(ec2Obj, sg.GroupId, cidrIp, sourceSgId, ipProtocol, fromPort, toPort, description);
-                console.log('Yooooooooooo..... authorized security group ingress');
+                logging('Yooooooooooo..... authorized security group ingress');
             }
         }
-
     } catch (error) {
         throw error;
     }
 }
 
-exports.handler = async (event) => {
-    try {
-        await handleOpeningAndClosingPorts(event);
-    } catch (error) {
-        throw error;
-    }
-}
+handleOpeningAndClosingPorts();
